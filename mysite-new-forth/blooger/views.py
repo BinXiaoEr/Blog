61d0c06f5c32bce1d617b511dcdd0cb216d63a91
BinXiaoEr <41658323+BinXiaoEr@users.noreply.github.com>
2019-06-09 21:44:54 +0800

    forth

---
+++ mysite-new-forth/blooger/views.py
@@ -0,0 +1,177 @@
+from django.shortcuts import render, reverse, redirect
+from .forms import *
+from django.http import JsonResponse
+from read_statistic.utils import get_seven_days_read_data, get_today_hot_data, get_yesterday_hot_data
+from django.core.cache import cache
+from django.contrib import auth
+from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
+from django.db.models import Sum
+from django.core.mail import send_mail
+from blog.models import *
+from .models import *
+import datetime
+import string
+import random
+import time
+
+def get_7_days_hot_blogs():
+    today = timezone.now().date()
+    date = today - datetime.timedelta(days=7)
+    blogs = Blog.objects \
+        .filter(read_details__date__lt=today, read_details__date__gte=date) \
+        .values('id', 'title') \
+        .annotate(read_num_sum=Sum('read_details__read_num')) \
+        .order_by('-read_num_sum')
+    return blogs[:7]
+
+
+def home(request):
+    blog_content_type = ContentType.objects.get_for_model(Blog)
+    dates, read_nums = get_seven_days_read_data(content_type=blog_content_type)
+    today_hot_data = get_today_hot_data(blog_content_type)
+    yesterday_hot_data = get_yesterday_hot_data(blog_content_type)
+    # seven_hot_data=get_7_days_hot_blogs()
+    # 获取7天热门博客的缓存数据
+    hot_blog_for_seven_days = cache.get('hot_blog_for_seven_days')
+    if hot_blog_for_seven_days is None:
+        hot_blog_for_seven_days = get_7_days_hot_blogs()
+        cache.set('hot_blog_for_seven_days', hot_blog_for_seven_days, 3600)
+    context = {'read_nums': read_nums, 'dates': dates,
+               'today_hot_data': today_hot_data,
+               'yesterday_hot_data': yesterday_hot_data,
+               'seven_hot_data': hot_blog_for_seven_days
+               }
+    return render(request, 'blogger/home.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            # clean函数在form中返回的是一个字典值含有user对象我们要用键取他的值
+            user = login_form.clean()
+            auth.login(request, user['user'])
+            return redirect(request.GET.get('from', reverse("blooger:home")))
+    else:
+        login_form = LoginForm()
+    context = {}
+    context['login_form'] = login_form
+    return render(request, 'blogger/login.html', context)
+
+
+# 详情页登陆模块
+def login_for_medal(request):
+    login_form = LoginForm(request.POST)
+    data = {}
+    if login_form.is_valid():
+        # clean函数在form中返回的是一个字典值含有user对象我们要用键取他的值
+        user = login_form.clean()
+        auth.login(request, user['user'])
+
+        data['status'] = 'SUCCESS'
+    else:
+        data['status'] = 'ERROR'
+    return JsonResponse(data)
+
+
+def register(request):
+    if request.method == 'POST':
+        reg_form = RegForm(request.POST)
+        if reg_form.is_valid():
+            username = reg_form.clean_username()
+            email = reg_form.clean_email()
+            password = reg_form.clean_password_again()
+            # 创建用户
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            # 登录用户
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse("blooger:home")))
+    else:
+        reg_form = RegForm()
+    context = {}
+    context['reg_form'] = reg_form
+    return render(request, 'blogger/register.html', context)
+
+#注销登陆
+def logout(request):
+    auth.logout(request)
+    return redirect(request.GET.get('from', reverse("blooger:home")))
+#用户信息
+def userinfo(request):
+    context={}
+    return render(request,'blogger/user_info.html',context)
+
+#修改用户信息
+def change_nickname(request):
+    redirect_to = request.GET.get('from', reverse('blooger:home'))
+    if request.method == 'POST':
+        form=ChangeNicknameForm(request.POST,user=request.user)
+        if form.is_valid():
+
+            nickname_new = form.cleaned_data['nickname_new']
+            print(nickname_new)
+            profile, created = Profile.objects.get_or_create(user=request.user)
+            profile.nickname=nickname_new
+            profile.save()
+            return redirect(redirect_to)
+    else:
+        form=ChangeNicknameForm()
+    context={}
+    context['page_title'] = '修改昵称'
+    context['form_title'] = '修改昵称'
+    context['submit_text'] = '修改'
+    context['form'] = form
+    context['return_back_url'] = redirect_to
+    return render(request, 'blogger/form.html', context)
+
+def bind_email(request):
+    redirect_to = request.GET.get('from', reverse('blooger:home'))
+
+    if request.method == 'POST':
+        form = BindEmailForm(request.POST, request=request)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            request.user.email = email
+            request.user.save()
+            return redirect(redirect_to)
+    else:
+        form = BindEmailForm()
+
+    context = {}
+    context['page_title'] = '绑定邮箱'
+    context['form_title'] = '绑定邮箱'
+    context['submit_text'] = '绑定'
+    context['form'] = form
+    context['return_back_url'] = redirect_to
+    return render(request, 'blogger/bind_email.html', context)
+
+
+def send_verification_code(request):
+    email = request.GET.get('email', '')
+    data = {}
+    if email != '':
+        # 生成验证码
+        code = ''.join(random.sample(string.ascii_letters + string.digits, 4))
+        now = int(time.time())
+        send_code_time = request.session.get('send_code_time', 0)
+        if now - send_code_time < 30:
+            data['status'] = 'ERROR'
+        else:
+            request.session['bind_email_code'] = code
+            request.session['send_code_time'] = now
+
+            # 发送邮件
+            send_mail(
+                '绑定邮箱',
+                '验证码：%s' % code,
+                '2872402050@qq.com',
+                [email],
+                fail_silently=False,
+            )
+            data['status'] = 'SUCCESS'
+    else:
+        data['status'] = 'ERROR'
+    return JsonResponse(data)
---
+++ mysite-new-forth/mysite/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url,include
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^comment/', include(('comment.urls', 'comment'), namespace='comment')),
+    url(r'^blog/',include(('blog.urls','blog'),namespace='blog')),
+    url(r'^likes/', include(('likes.urls', 'likes'), namespace='likes')),
+    url(r'^', include(('blooger.urls', 'blooger'), namespace='blooger')),
+    url(r'mdeditor/', include('mdeditor.urls')),
+]
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
